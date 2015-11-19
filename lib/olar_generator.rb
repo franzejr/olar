c2c6be2126abd789485a44309ac4d1a42799e771
Franze Jr <franzejr@gmail.com>
2015-11-18 22:38:54 -0300

    Add New rules and tests

--- lib/olar_generator.rb
+++ lib/olar_generator.rb
@@ -2,17 +2,21 @@ class OlarGenerator
   def self.olar(content)
     ''.tap do |s|
       content.split(' ').each do |word|
-        s << add_point(add_r(word))
+        s << add_point(add_r(add_mais(word)))
       end
     end
   end
 
   def self.add_r(word)
-    word =~ /\w+(a|e|o|i)\z/ ? "#{word}r" : word
+    word =~ /\w+(a|e|o)\z/ ? "#{word}r" : word
   end
 
   def self.add_point(word)
-    word << '.'
+    word =~ /\w+(\!|\?|\.|\,)\z/ ? word : "#{word}."
+  end
+
+  def self.add_mais(word)
+    word =~ /mas/ ? 'mais' : word
   end
 
 end
--- spec/lib/olar_generator_spec.rb
+++ spec/lib/olar_generator_spec.rb
@@ -6,18 +6,27 @@ describe OlarGenerator do
 
     describe '.olar' do
       context 'it does olar!' do
-        it '' do
-          expect(gen.olar('Oi, meu amigo')).to eq 'Oi,.meu.amigor.'
+        it 'runs all the olar rules and return the string' do
+          expect(gen.olar('Oi, meu amigo')).to eq 'Oi,meu.amigor.'
+        end
+      end
+    end
+
+    describe '.add_mais' do
+      context 'when it receives mais' do
+        it 'changes mas to mais' do
+          expect(gen.add_mais('mas')).to eq 'mais'
         end
       end
 
-      context 'when it finishes with a special character' do
-
+      context 'when it does not receive mais' do
+        it 'changes mas to mais' do
+          expect(gen.add_mais('olar')).to eq 'olar'
+        end
       end
     end
 
     describe '.add_r' do
-
       context 'when adds successfully' do
         it { expect(gen.add_r('amigo')).to eq 'amigor' }
         it { expect(gen.add_r('amiga')).to eq 'amigar' }
@@ -32,6 +41,16 @@ describe OlarGenerator do
     end
 
     describe '.add_point' do
-      it { expect(gen.add_point('amigo')).to eq 'amigo.' }
+      context 'when it finishes with a non-special character' do
+        it 'adds a point at the end' do
+          expect(gen.add_point('amigo')).to eq 'amigo.'
+        end
+      end
+
+      context 'when it finishes with a special character' do
+        it 'returns the word itself' do
+          expect(gen.add_point('amigo!')).to eq 'amigo!'
+        end
+      end
     end
 end
