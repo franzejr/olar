71edd04f920457378e34f716c88ea7fc51f04807
franzejr <franzejr@gmail.com>
2015-11-19 10:58:12 -0300

    Change OlarGenerator to Build Pattern

--- lib/olar_generator.rb
+++ lib/olar_generator.rb
@@ -1,22 +1,45 @@
 class OlarGenerator
-  def self.olar(content)
-    ''.tap do |s|
-      content.split(' ').each do |word|
-        s << add_point(add_r(add_mais(word)))
+
+  def initialize(phrase)
+    @phrase = phrase
+    @stack =  {}
+  end
+
+  def result
+    olar = transform_into_olar_array
+    rebuild_phrase olar
+  end
+
+  def apply_r
+    @stack[__method__] = Proc.new do |word|
+        word =~ /\w+(a|e|o)\z/ ? "#{word}r" : word
+    end
+    self
+  end
+
+  def apply_mais
+    @stack[__method__] = Proc.new do |word|
+        word =~ /M|mas/ ? 'mais' : word
+    end
+    self
+  end
+
+  private
+
+  def transform_into_olar_array
+    @phrase.split(' ').map do |word|
+      applied_rule = word
+      @stack.each do |method_name, proc_method|
+        applied_rule = proc_method.call(applied_rule)
       end
+      applied_rule
     end
   end
 
-  def self.add_r(word)
-    word =~ /\w+(a|e|o)\z/ ? "#{word}r" : word
-  end
-
-  def self.add_point(word)
-    word =~ /\w+(\!|\?|\.|\,)\z/ ? word : "#{word}."
-  end
-
-  def self.add_mais(word)
-    word =~ /mas/ ? 'mais' : word
+  def rebuild_phrase(words)
+    words.map { |word|
+        word =~ /\w+(\!|\?|\.|\,)\z/ ? word : "#{word}."
+    }.join('').chomp '.'
   end
 
 end
